0ab976144c3cf2f5b91b2e940c7cb00896d5902b
george.emad <george.emad9819@gmail.com>
2024-09-09 13:05:18 +0300

    add in mem repo

--- EndPoints/GamesEndPoints.cs
+++ EndPoints/GamesEndPoints.cs
@@ -1,49 +1,36 @@
 
 using GameStore.Api.Entities;
+using GameStore.Api.Repository;
 
 namespace GameStore.Api.EndPoints;
 
 public static class GameEndPoints
 {
     const string GetGameEndPointName = "GetGameById";
-    static List<Game> games = new(){
-    new Game{
-        Id = 1,
-        Name = "Game1",
-        Genre = "Action",
-        ImageUri = "https://placehold.co/100"
-    },
-    new Game{
-        Id = 2,
-        Name = "Game2",
-        Genre = "Fantasy",
-        ImageUri = "https://placehold.co/100"
-    },
-};
 
     public static RouteGroupBuilder MapGameEndPoints(this IEndpointRouteBuilder route)
     {
+        IGamesRepository repo = new InMemGamesRepository();
         var gameGroup = route.MapGroup("/games")
                     .WithParameterValidation();
 
-        gameGroup.MapGet("/", () => games);
+        gameGroup.MapGet("/", async () => await repo.GetAllAsync());
 
-        gameGroup.MapPost("/", (Game game) =>
+        gameGroup.MapPost("/", async (Game game) =>
         {
             if (game is null)
             {
                 return Results.BadRequest();
             }
-            int id = games.Max(x => x.Id) + 1;
-            game.Id = id;
-            games.Add(game);
+            
+            await repo.CreateAsync(game);
 
             return Results.CreatedAtRoute(GetGameEndPointName, new { id = game.Id }, game);
         });
 
-        gameGroup.MapGet("/{id}", (int id) =>
+        gameGroup.MapGet("/{id}", async (int id) =>
         {
-            Game? game = games.Find((x) => x.Id == id);
+            Game? game = await repo.GetAsync(id);
 
             if (game is null)
             {
